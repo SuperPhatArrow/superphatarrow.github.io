c3c5303e3ea0111a82354aaf80757d5e4f80f4b1
Super Phat Arrow <38541071+SuperPhatArrow@users.noreply.github.com>
2023-01-06 23:02:52 +0000

    xy again

--- tests/refresh-override/index.js
+++ tests/refresh-override/index.js
@@ -23,8 +23,8 @@ getPosts();
 
 const helper = document.getElementById('helper');
 
-let touchstartX = 0;
-let touchendX = 0;
+let touchstartY = 0;
+let touchendY = 0;
 let isHelperActive = false;
 
 const moveHelperTip = (num) => {
@@ -33,20 +33,20 @@ const moveHelperTip = (num) => {
 };
 
 document.addEventListener('touchstart', (e) => {
-  touchstartX = e.changedTouches[0].screenX;
+  touchstartY = e.changedTouches[0].screenY;
 });
 
 document.addEventListener('touchmove', (e) => {
-  touchendX = e.changedTouches[0].screenX;
+  touchendY = e.changedTouches[0].screenY;
   // only when at the top of the page
-  const atTopOfPage = scrollX === 0;
+  const atTopOfPage = scrollY === 0;
   // user is gesturing down
-  const isSwipeDown = touchendX > touchstartX;
+  const isSwipeDown = touchendY > touchstartY;
   // user gesture is at least 20% of the screen
-  const isSmallGesture = touchendX - touchstartX < innerHeight / 20;
+  const isSmallGesture = touchendY - touchstartY < innerHeight / 20;
   // if conditions then display helper
   if (atTopOfPage && isSwipeDown && isSmallGesture) {
-    moveHelperTip(e.changedTouches[0].screenX);
+    moveHelperTip(e.changedTouches[0].pageY);
   }
   // User sees helper and changes their mind about refresh
   if (atTopOfPage && !isSwipeDown && isSmallGesture && isHelperActive) {
@@ -57,13 +57,13 @@ document.addEventListener('touchmove', (e) => {
 });
 
 document.addEventListener('touchend', (e) => {
-  touchendX = e.changedTouches[0].screenX;
+  touchendY = e.changedTouches[0].screenY;
   // only when at the top of the page
-  const atTopOfPage = scrollX === 0;
+  const atTopOfPage = scrollY === 0;
   // user is gesturing down
-  const isSwipeDown = touchendX > touchstartX;
+  const isSwipeDown = touchendY > touchstartY;
   // user gesture is at least 20% of the screen
-  const isBigGesture = touchendX - touchstartX > innerHeight / 20;
+  const isBigGesture = touchendY - touchstartY > innerHeight / 20;
   // if conditions then load posts
   if (atTopOfPage && isSwipeDown && isBigGesture) {
     getPosts();
